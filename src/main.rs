8d26771a2ae4835b11ad010716292b01f041b876
Krithin Sitaram <krithin@gmail.com>
2020-12-19 01:21:40 +0800

    Second star.

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ fn main() {
     for line in stdin.lock().lines() {
         let line = line.unwrap();
         let parts: Vec<&str> = line.split(' ').collect();
-        let limits: Vec<usize> = parts[0].split('-').map(|n| n.parse::<usize>().unwrap()).collect();
+        let required_positions: Vec<usize> = parts[0].split('-').map(|n| n.parse::<usize>().unwrap()).collect();
         let required_char = parts[1].chars().next().unwrap();
-        let test_string = parts[2];
 
-        let count = test_string.chars().filter(|&c| c == required_char).count();
-        if count >= limits[0] && count <= limits[1] {
+        let mut test_chars = parts[2].chars();
+        let first_char = test_chars.nth(required_positions[0] - 1).unwrap();
+        let second_char = test_chars.nth(required_positions[1] - required_positions[0] - 1).unwrap();
+
+        if (first_char == required_char) ^ (second_char == required_char) {
             valid_count += 1;
         }
     }
