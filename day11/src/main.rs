bf5e89740a3fb740cb80898bbb628c6266c61d32
Krithin Sitaram <krithin@gmail.com>
2020-12-21 00:16:27 +0800

    Day 11 Part 2.

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,30 +1,40 @@
 use std::{io, io::prelude::*};
 use std::{thread};
 
+fn first_along_direction(seatmap: &Vec<Vec<char>>, i: usize, j: usize, dir_update: &fn(i32, i32) -> (i32, i32)) -> char {
+    let mut i: i32 = i as i32;
+    let mut j: i32 = j as i32;
+    loop {
+        // Because we don't have destructuring assignment yet.
+        let (newi, newj) = dir_update(i, j);
+        i = newi;
+        j = newj;
+
+        if i < 0 || j < 0 || i >= seatmap.len() as i32 || j >= seatmap[i as usize].len() as i32 {
+            // We hit a wall
+            break '-';
+        }
+        match seatmap[i as usize][j as usize] {
+            '.' => {},
+            'L' => break 'L',
+            '#' => break '#',
+            _ => panic!("Bad cell in seat map!"),
+        }
+    }
+}
+
 fn count_adj_occupied(seatmap: &Vec<Vec<char>>, i: usize, j: usize) -> u8 {
     let mut count: u8 = 0;
-    if i > 0 {
-        if j > 0 {
-            count += match seatmap[i-1][j-1] { '#' => 1, _ => 0 };
-        }
-        count += match seatmap[i-1][j] { '#' => 1, _ => 0 };
-        if j < seatmap[i].len() - 1 {
-            count += match seatmap[i-1][j+1] { '#' => 1, _ => 0 };
-        }
-    }
-    if j > 0 {
-        count += match seatmap[i][j-1] { '#' => 1, _ => 0 };
-    }
-    if j < seatmap[i].len() - 1 {
-        count += match seatmap[i][j+1] { '#' => 1, _ => 0 };
-    }
-    if i < seatmap.len() - 1 {
-        if j > 0 {
-            count += match seatmap[i+1][j-1] { '#' => 1, _ => 0 };
-        }
-        count += match seatmap[i+1][j] { '#' => 1, _ => 0 };
-        if j < seatmap[i].len() - 1 {
-            count += match seatmap[i+1][j+1] { '#' => 1, _ => 0 };
+    let dir_update_functions: Vec<&fn(i32, i32) -> (i32, i32)> = [
+        |i, j| (i-1, j-1), |i, j| (i-1, j), |i, j| (i-1, j+1),
+        |i, j| (i, j-1), |i, j| (i, j+1),
+        |i, j| (i+1, j-1), |i, j| (i+1, j), |i, j| (i+1, j+1),
+    ].iter().collect();
+    for f in dir_update_functions {
+        count += match first_along_direction(seatmap, i, j, f) {
+            '-'|'L' => 0,
+            '#' => 1,
+            _ => panic!("Bad kind of nearest seat along direction"),
         }
     }
     return count;
@@ -43,8 +53,8 @@ fn tick(seatmap: &Vec<Vec<char>>) -> (Vec<Vec<char>>, bool) {
                     _ => panic!("unexpected adj count"),
                 },
                 '#' => match count_adj_occupied(seatmap, i, j) {
-                    0..=3 => '#',
-                    4..=8 => {changed = true; 'L'},
+                    0..=4 => '#',
+                    5..=8 => {changed = true; 'L'},
                     _ => panic!("unexpected adj count"),
                 },
                 _ => panic!("Unexpected char in seat map"),
@@ -76,8 +86,8 @@ fn main() {
     let final_map = loop {
         let (next_map, changed) = tick(&seatmap);
 
-        //debug_print(&next_map);
-        //thread::sleep_ms(10);
+        debug_print(&next_map);
+        thread::sleep_ms(10);
         if !changed { break next_map };
         seatmap = next_map;
     };
