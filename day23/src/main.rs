2651ee84059c2808cea633bb6473951d1d03686f
Krithin Sitaram <krithin@gmail.com>
2020-12-23 17:18:41 +0800

    Day 23 Part 1: reimplemented as a linked list.

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,48 +1,64 @@
-use std::collections::VecDeque;
+fn next_cup(cups: &[u32], label: u32) -> u32 {
+    return cups[label as usize];
+}
+
+// removes the cup _following_ label, and returns the label of the removed cup
+fn remove_next(cups: &mut [u32], label: u32) -> u32 {
+    let c = next_cup(cups, label);
+    cups[label as usize] = next_cup(cups, c);
+    cups[c as usize] = 0;
+    return c;
+}
+
+// inserts a cup after the one with the given label
+fn insert_after(cups: &mut [u32], position: u32, new_label: u32) {
+    let c = next_cup(cups, position);
+    cups[position as usize] = new_label;
+    cups[new_label as usize] = c;
+}
 
 fn main() {
-
     let input = "586439172";
     //let input = "389125467";
+    let input: Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap() as u32).collect();
 
-    let mut seq: VecDeque<u32> = input.chars().map(|c| c.to_digit(10).unwrap() as u32).collect();
-    // for i in seq.len()+1..=1000000 {
-    //     seq.push_back(i as u32);
-    // }
-    let mut dst: usize;
-    let num_moves = 100;
-    let max_target = seq.len() as u32;
+    const NUM_CUPS: usize = 9;
+    let mut cups: [u32; NUM_CUPS + 1] = [0; NUM_CUPS + 1];
 
-    for move_num in 1..=num_moves {
-        if move_num % 1000 == 0 { println!("{}", move_num);}
+    for i in 0..input.len()-1 {
+        cups[input[i] as usize] = input[i+1];
+    }
+    cups[input[input.len() - 1] as usize] = input[0];
 
-        let current: u32 = seq.pop_front().unwrap();
-        let mut target = current - 1;
-        seq.push_back(current);
+    let mut curr = input[0];
+    let mut target: u32;
+    const NUM_MOVES: u32 = 100;
+    for _i in 1..=NUM_MOVES {
+        target = curr-1;
+ 
+        let mut removed_cups: Vec<u32> = Vec::new();
+        for _j in 1..=3 {
+            removed_cups.push(remove_next(&mut cups, curr));
+        }
 
-        let three: VecDeque<u32> = seq.drain(0..3).collect();
-
-        println!("target: {}, seq: {:?}, three: {:?}", target, seq, three);
-        if target < 1 { target = max_target; }
-        while three.contains(&target) {
+        if target < 1 { target = NUM_CUPS as u32 }
+        while removed_cups.contains(&target) {
             target -= 1;
-            if target < 1 { target = max_target; }
+            if target < 1 { target = NUM_CUPS as u32 }
         }
 
-        dst = 0;
-        for (i, &val) in seq.iter().enumerate() {
-            if val == target {
-                dst = i + 1;
-            }
-        }
-        println!("final target: {}, seq: {:?}, dst: {}", target, seq, dst);
-
-        for &i in three.iter().rev() {
-            seq.insert(dst, i);
+        for new_label in removed_cups {
+            insert_after(&mut cups, target, new_label);
+            target = new_label;
         }
 
-        println!("{:?}", seq);
+        curr = next_cup(&cups, curr);
     }
 
-    println!("{:?}", seq);
+    let mut i = next_cup(&cups, 1);
+    while i != 1 {
+        print!("{}", i);
+        i = next_cup(&cups, i);
+    }
+    println!("");
 }
