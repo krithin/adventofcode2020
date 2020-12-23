f05e20d15a768092e364d8b4d74012a5a6b106fa
Krithin Sitaram <krithin@gmail.com>
2020-12-23 17:36:10 +0800

    Update for part 2.
    Also convert some u32s to usizes so we don't cast as much.

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,50 +1,58 @@
-fn next_cup(cups: &[u32], label: u32) -> u32 {
-    return cups[label as usize];
+fn next_cup(cups: &[usize], label: usize) -> usize {
+    return cups[label];
 }
 
 // removes the cup _following_ label, and returns the label of the removed cup
-fn remove_next(cups: &mut [u32], label: u32) -> u32 {
+fn remove_next(cups: &mut [usize], label: usize) -> usize {
     let c = next_cup(cups, label);
-    cups[label as usize] = next_cup(cups, c);
-    cups[c as usize] = 0;
+    cups[label] = next_cup(cups, c);
+    cups[c] = 0;
     return c;
 }
 
 // inserts a cup after the one with the given label
-fn insert_after(cups: &mut [u32], position: u32, new_label: u32) {
+fn insert_after(cups: &mut [usize], position: usize, new_label: usize) {
     let c = next_cup(cups, position);
-    cups[position as usize] = new_label;
-    cups[new_label as usize] = c;
+    cups[position] = new_label;
+    cups[new_label] = c;
 }
 
 fn main() {
     let input = "586439172";
     //let input = "389125467";
-    let input: Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap() as u32).collect();
+    let input: Vec<usize> = input.chars().map(|c| c.to_digit(10).unwrap() as usize).collect();
 
-    const NUM_CUPS: usize = 9;
-    let mut cups: [u32; NUM_CUPS + 1] = [0; NUM_CUPS + 1];
+    const NUM_CUPS: usize = 1000000;
+    let mut cups: [usize; NUM_CUPS + 1] = [0; NUM_CUPS + 1];
 
     for i in 0..input.len()-1 {
-        cups[input[i] as usize] = input[i+1];
+        cups[input[i]] = input[i+1];
     }
-    cups[input[input.len() - 1] as usize] = input[0];
+    cups[input[input.len()-1]] = input.len() + 1;
+    for i in input.len()+1..=NUM_CUPS-1 {
+        cups[i] = i+1;
+    }
+    cups[NUM_CUPS] = input[0];
+
+    //println!("{:?}", cups);
 
     let mut curr = input[0];
-    let mut target: u32;
-    const NUM_MOVES: u32 = 100;
-    for _i in 1..=NUM_MOVES {
+    let mut target: usize;
+    const NUM_MOVES: u32 = 10000000;
+    for i in 1..=NUM_MOVES {
+        if i % 10000 == 0 { println!("move {}", i)}
+
         target = curr-1;
  
-        let mut removed_cups: Vec<u32> = Vec::new();
+        let mut removed_cups: Vec<usize> = Vec::new();
         for _j in 1..=3 {
             removed_cups.push(remove_next(&mut cups, curr));
         }
 
-        if target < 1 { target = NUM_CUPS as u32 }
+        if target < 1 { target = NUM_CUPS }
         while removed_cups.contains(&target) {
             target -= 1;
-            if target < 1 { target = NUM_CUPS as u32 }
+            if target < 1 { target = NUM_CUPS }
         }
 
         for new_label in removed_cups {
@@ -55,10 +63,7 @@ fn main() {
         curr = next_cup(&cups, curr);
     }
 
-    let mut i = next_cup(&cups, 1);
-    while i != 1 {
-        print!("{}", i);
-        i = next_cup(&cups, i);
-    }
-    println!("");
+    let cup1 = next_cup(&cups, 1);
+    let cup2 = next_cup(&cups, cup1);
+    println!("{} {} {}", cup1, cup2, cup1 * cup2);
 }
