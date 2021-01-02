2c5648582f493c9f78a84e7bce93f2759a11287b
Krithin Sitaram <krithin@gmail.com>
2021-01-03 02:48:47 +0800

    Day 22 Part 2 - code cleanup now that we're using Vec everywhere.

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,64 +1,47 @@
 use std::{io, io::prelude::*};
-use std::collections::{HashSet, VecDeque};
+use std::collections::HashSet;
 
-fn play_round(deck0: &mut VecDeque<u8>, deck1: &mut VecDeque<u8>) {
-    println!("{:?}\n{:?}\n", deck0, deck1);
-    let card0 = deck0.pop_front().unwrap();
-    let card1 = deck1.pop_front().unwrap();
+fn play_round(deck0: &mut Vec<u8>, deck1: &mut Vec<u8>) {
+    //println!("{:?}\n{:?}\n", deck0, deck1);
+    let card0 = deck0.drain(..=0).next().unwrap();
+    let card1 = deck1.drain(..=0).next().unwrap();
 
     if card0 as usize > deck0.len() || card1 as usize > deck1.len() {
         if card0 > card1 {
-            deck0.push_back(card0);
-            deck0.push_back(card1);
+            deck0.push(card0);
+            deck0.push(card1);
         } else {
-            deck1.push_back(card1);
-            deck1.push_back(card0);
+            deck1.push(card1);
+            deck1.push(card0);
         }
     } else {
-        println!("Starting new subgame with subdecks.");
-        let mut subdeck0: Vec<u8> = Vec::new();
-        let mut subdeck1: Vec<u8> = Vec::new();
-        for (i, val) in deck0.iter().enumerate() {
-            if i < card0 as usize {
-                subdeck0.push(*val);
-            } else {
-                break;
-            }
-        }
-        for (i, val) in deck1.iter().enumerate() {
-            if i < card1 as usize {
-                subdeck1.push(*val);
-            } else {
-                break;
-            }
-        }
-        let (newdeck0, newdeck1) = play_game(&subdeck0, &subdeck1);
+        //println!("Starting new subgame with subdecks.");
+        let (newdeck0, newdeck1) = play_game(&deck0[..card0 as usize], &deck1[..card1 as usize]);
         if newdeck1.len() == 0 {
-            deck0.push_back(card0);
-            deck0.push_back(card1);
+            deck0.push(card0);
+            deck0.push(card1);
         } else if newdeck0.len() == 0 {
-            deck1.push_back(card1);
-            deck1.push_back(card0);
+            deck1.push(card1);
+            deck1.push(card0);
         }
     }
 }
 
-fn play_game(deck0: &[u8], deck1: &[u8]) -> (VecDeque<u8>, VecDeque<u8>) {
-    let mut deck0: VecDeque<u8> = deck0.iter().cloned().collect();
-    let mut deck1: VecDeque<u8> = deck1.iter().cloned().collect();
+fn play_game(deck0: &[u8], deck1: &[u8]) -> (Vec<u8>, Vec<u8>) {
+    let mut deck0: Vec<u8> = deck0.iter().cloned().collect();
+    let mut deck1: Vec<u8> = deck1.iter().cloned().collect();
     let mut seen_states: HashSet<(Vec<u8>, Vec<u8>)> = HashSet::new();
 
     loop {
-        if seen_states.contains(&(deck0.iter().cloned().collect(), deck1.iter().cloned().collect())) {
-            println!("Game over! Player 1 wins because we're in a seen state.");
-            return (deck0, VecDeque::new());
+        if seen_states.contains(&(deck0.clone(), deck1.clone())) {
+            // println!("Game over! Player 1 wins because we're in a seen state.");
+            return (deck0, Vec::new());
         } else if deck0.is_empty() || deck1.is_empty() {
-            if deck0.is_empty() { println!("Game over! Player 2 won") } else {println!("Game over! Player 1 won")};
+            // if deck0.is_empty() { println!("Game over! Player 2 won") } else {println!("Game over! Player 1 won")};
             return (deck0, deck1);
         } else {
-            seen_states.insert((deck0.iter().cloned().collect(), deck1.iter().cloned().collect()));
+            seen_states.insert((deck0.clone(), deck1.clone()));
             play_round(&mut deck0, &mut deck1);
-            //thread::sleep(time::Duration::from_millis(1));
         }
     }
 }
